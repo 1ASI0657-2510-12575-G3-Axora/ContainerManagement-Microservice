d609231ccfcc7bd71d4ab2385cd35a8556c87763
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-06 15:54:44 -0500

    feat(main):Add application command

---
+++ ContainerManagement-Microservice/ContainerManagement/Application/Commands/UpdateContainerMetricsCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DittoBox.API.ContainerManagement.Application.Commands
+{
+    public record UpdateContainerMetricsCommand (
+        [Required]double Temperature,
+        [Required]double Humidity,
+        [Required]double Oxygen,
+        [Required]double Dioxide,
+        [Required]double Ethylene,
+        [Required]double Ammonia,
+        [Required]double SulfurDioxide
+    );
+}
