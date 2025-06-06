8c3f053e42521f77306f3a295ec90e376a12aa42
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-06 15:58:35 -0500

    feat(main):Add interface

---
+++ ContainerManagement-Microservice/ContainerManagement/Interface/Resources/CreateContainerResource.cs
@@ -0,0 +1,22 @@
+﻿using DittoBox.API.ContainerManagement.Domain.Models.Entities;
+
+namespace DittoBox.API.ContainerManagement.Interface.Resources
+{
+	public record CreateContainerResource(
+			int Id,
+			string Uuid,
+			string Name,
+			string Description
+		)
+	{
+		public static CreateContainerResource FromContainer(Container container)
+		{
+			return new CreateContainerResource(
+				container.Id,
+				container.Uiid,
+				container.Name,
+				container.Description
+			);
+		}
+	}
+}
